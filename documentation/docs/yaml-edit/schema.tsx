253c0e378ef9fc8c2a2c8c47a2f4b9465f743bd3
Anders Fredrik Kiær <31612826+anders-kiaer@users.noreply.github.com>
2025-07-01 10:01:50 +0000

    create gh-pages

--- documentation/docs/yaml-edit/schema.tsx
+++
@@ -1,5 +0,0 @@
-import { type RJSFSchema } from "@rjsf/utils";
-import sim2seisJsonSchema from "./fmu-sim2seis-schema.json";
-
-// @ts-ignore
-export const sim2seisSchema: RJSFSchema = sim2seisJsonSchema;
