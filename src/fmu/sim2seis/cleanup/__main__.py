bbb3abce5ec1604b037ff499238fb18c2afac715
hflesche <hfle@equinor.com>
2026-07-06 15:05:46 +0200

    doc: update the documentation for sim2seis_cleanup, remove inaccurate docstring

--- src/fmu/sim2seis/cleanup/__main__.py
+++ src/fmu/sim2seis/cleanup/__main__.py
@@ -1,22 +1,4 @@
-"""
-The fmu-sim2seis workflow generates a number of pickle-files, which contains
-the original class objects, rather than derived outputs. Although they are valuable for
-inspection after the sim2seis workflow is run, they require significant disk space, and
-as such should be removed when they are no longer needed. A separate ERT workflow is
-added for this purpose. This can also be run from command line for interactive sessions.
 
-Command line call:
-
-    sim2seis_cleanup --config-file <...>
-
-    --config-file: yaml-file with configuration parameters; the path is split into
-                  directory and file name automatically. The directory is expected
-                  to be `sim2seis/model` in an fmu directory structure.
-    This routine can be used both for the synthetic modelling performed by fmu-sim2seis,
-    or for the observed data. When the observed data is processed, a number of pickle
-    files are also generated. To remove these, give the path and name to the observed
-    data config file.
-"""
 
 import sys
 from collections.abc import Iterator
