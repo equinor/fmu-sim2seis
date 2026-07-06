bea79d354d4f6826cb32073393494e51ba1f69ee
hflesche <hfle@equinor.com>
2026-07-06 15:20:03 +0200

    feat: add seismic option to ert model of sim2seis_cleanup

--- src/fmu/sim2seis/forward_models/sim2seis_cleanup.py
+++ src/fmu/sim2seis/forward_models/sim2seis_cleanup.py
@@ -17,6 +17,8 @@ class Cleanup(ForwardModelStepPlugin):
                 "sim2seis_cleanup",
                 "--config-file",
                 "<CONFIG_FILE>",
+                "--include_seismic",
+                "<INCLUDE_SEISMIC>",
             ],
         )
 
@@ -35,9 +37,8 @@ class Cleanup(ForwardModelStepPlugin):
             source_package="fmu.sim2seis",
             source_function_name="Cleanup",
             description="",
-            examples=(
-                "code-block:: console\n\n"
-                "FORWARD_MODEL CLEANUP("
-                "<CONFIG_FILE>=<RUNPATH>/sim2seis/model/sim2seis_combined_config.yml, "
-            ),
+            examples="""
+code-block:: console\n\n"
+                "<CONFIG_FILE>=<RUNPATH>/sim2seis/model/sim2seis_combined_config.yml, <INCLUDE_SEISMIC>=false
+""",  # noqa: E501
         )
